b97f9834573c90ae081e787c7cadfb153b54366c
Terrence Mahnken <terrencemm2@gmail.com>
2020-11-16 22:08:43 -0600

    adds screenshot file

--- src/utils/dataHandling.js
+++
@@ -1,8 +0,0 @@
-export const limitResults = (data, offset) => {
-    const index = offset * 10
-    return data.slice(index, (index + 10))
-}
-
-export const pagination = (num) => {
-    return Math.ceil(num / 10)
-}
